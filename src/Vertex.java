fe61e4bc634d1cac0ea00436e948b050c8992f87
timzehnle <zehnletim@googlemail.com>
2024-09-12 15:31:22 +0200

    12.09

--- src/Vertex.java
+++ src/Vertex.java
@@ -9,21 +9,32 @@ class Vertex{
     public Vertex(double x, double y){
         this.x=x;
         this.y=y;
-        j=136364886;
         
     }       
-        public static void main(String[] args){  
+        public static void main(String[] args){
             Vertex v1 = new Vertex(42, 17);
             Vertex v2 = new Vertex(0.3, 24.6);
+            Vertex v3 = v1.skalarMult(2);
+
             System.out.println(v1);
+            System.out.println(v3);
+            System.out.println(v1.lenght());
+            System.out.println(v2.lenght());
+
             
-            while(v1.x < j){
-                v1.x=v1.x+v2.x;
-                v1.y=v1.y+v2.y;
-                if(v1.x <= j){
-                    System.out.println(v1);
-                }
-            }
+        }
+
+        public Vertex skalarMult(double s){
+            return new Vertex(x*s,y*s);
+        }
+
+        public void skalarMultMod (double s){
+            x=x*s; 
+            y=y*s;
+        }
+
+        public double lenght(){
+            return Math.sqrt(x*x+y*y);
         }
     
         public String toString() {
