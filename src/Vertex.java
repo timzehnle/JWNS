02c0df6aba771d0259b624a366c73709eea9f5ae
timzehnle <zehnletim@googlemail.com>
2024-09-12 14:43:17 +0200

    Rechnungsübungen

--- src/Vertex.java
+++ src/Vertex.java
@@ -4,21 +4,25 @@ class Vertex{
     public double x;
     public double y;
     public static int i;
+    public static int j;
 
     public Vertex(double x, double y){
         this.x=x;
         this.y=y;
+        j=136364886;
         
     }       
         public static void main(String[] args){  
             Vertex v1 = new Vertex(42, 17);
             Vertex v2 = new Vertex(0.3, 24.6);
             System.out.println(v1);
-
-            for(i=0; i<9 ; i++){
+            
+            while(v1.x < j){
                 v1.x=v1.x+v2.x;
                 v1.y=v1.y+v2.y;
-                System.out.println(v1);
+                if(v1.x <= j){
+                    System.out.println(v1);
+                }
             }
         }
     
