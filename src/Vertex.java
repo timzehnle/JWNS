334702868c10503d686afb6723d49c41264e4760
timzehnle <zehnletim@googlemail.com>
2024-09-17 09:02:22 +0200

    17.9

--- src/Vertex.java
+++ src/Vertex.java
@@ -11,34 +11,58 @@ class Vertex{
         this.y=y;
         
     }       
-        public static void main(String[] args){
-            Vertex v1 = new Vertex(42, 17);
-            Vertex v2 = new Vertex(0.3, 24.6);
-            Vertex v3 = v1.skalarMult(2);
-
-            System.out.println(v1);
-            System.out.println(v3);
-            System.out.println(v1.lenght());
-            System.out.println(v2.lenght());
-
-            
-        }
-
-        public Vertex skalarMult(double s){
-            return new Vertex(x*s,y*s);
-        }
-
-        public void skalarMultMod (double s){
-            x=x*s; 
-            y=y*s;
-        }
-
-        public double lenght(){
-            return Math.sqrt(x*x+y*y);
-        }
     
-        public String toString() {
-            return "("+x+","+y+")";
+    public Vertex skalarMult(double s){
+        return new Vertex(x*s,y*s);
     }
-  
+
+    public void skalarMultMod (double s){
+        x=x*s; 
+        y=y*s;
+    }
+
+    public double lenght(){
+        return Math.sqrt(x*x+y*y);
+    }
+
+    public void setX(double x){
+        this.x=x;
+    }
+
+    public void setY(double y){
+        this.y=y;
+    }
+
+    public double getX(){
+        return x;
+    }
+
+    public double getY(){
+        return y;
+    }
+
+    public Vertex add(Vertex v2){
+        return new Vertex(x+v2.x, y+v2.y);
+    }
+
+    public void addMod(Vertex v2){
+        x=x+v2.x;
+        y=y+v2.y;
+    }
+
+    public String toString() {
+        return "("+x+","+y+")";
+
+    }
+
+    public boolean equals (Object thatObject){
+        if(thatObject instanceof Vertex){
+            Vertex that = (Vertex) thatObject;
+            
+            return this.x == that.x && this.y == that.y;
+        }
+        
+        return false;
+
+    }
 }
